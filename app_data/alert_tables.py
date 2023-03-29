fee6cea1e77a10765807a2311a25f9e5d0df9c60
Jack Consoli <jack_consoli@yahoo.com>
2023-03-29 16:48:11 -0400

    Add files via upload

--- app_data/alert_tables.py
+++ app_data/alert_tables.py
@@ -1,4 +1,4 @@
-# Copyright 2019, 2020, 2021, 2022 Jack Consoli.  All rights reserved.
+# Copyright 2019, 2020, 2021, 2022, 2023 Jack Consoli.  All rights reserved.
 #
 # NOT BROADCOM SUPPORTED
 #
@@ -15,6 +15,12 @@
 
 """
 :mod:`alert_tables` - Alert definitions for class AlertTable
+
+**Description**
+
+    Defines how alerts are displayed. It could probably be reduced to one simple dictionary. It started as something
+    more complicated. I left it this way because other code references the class herein. Adding lookup_d was an after
+    thought.
 
 Version Control::
 
@@ -53,16 +59,18 @@ Version Control::
     +-----------+---------------+-----------------------------------------------------------------------------------+
     | 3.1.1     | 25 Jul 2022   | Improved error message LOGIN_DUP_LOGIN                                            |
     +-----------+---------------+-----------------------------------------------------------------------------------+
+    | 3.1.2     | 01 Jan 2023   | Added GENERAL_BASE alerts and ALIAS_INITIATOR_UPPER                               |
+    +-----------+---------------+-----------------------------------------------------------------------------------+
 """
 
 __author__ = 'Jack Consoli'
-__copyright__ = 'Copyright 2019, 2020, 2021, 2022 Jack Consoli'
-__date__ = '25 Jul 2022'
+__copyright__ = 'Copyright 2019, 2020, 2021, 2022, 2023 Jack Consoli'
+__date__ = '01 Jan 2023'
 __license__ = 'Apache License, Version 2.0'
 __email__ = 'jack.consoli@broadcom.com'
 __maintainer__ = 'Jack Consoli'
 __status__ = 'Released'
-__version__ = '3.1.1'
+__version__ = '3.1.2'
 
 import brcddb.classes.alert as al
 
@@ -88,12 +96,13 @@ class ALERT_NUM:
     # Switch level alerts
     SWITCH_BASE = 300
     SWITCH_IDID = SWITCH_BASE + 1
-    SWITCH_FIRMWARE_8_2 = SWITCH_IDID + 1
+    SWITCH_FIRMWARE_8_2 = SWITCH_IDID + 1  # Deprecated 6 Dec 2022
     HANDLE_DUP_WWN = SWITCH_FIRMWARE_8_2 + 1
     SWITCH_ISL_IMBALANCE = HANDLE_DUP_WWN + 1
     SWITCH_ISL_BW = SWITCH_ISL_IMBALANCE + 1
     SWITCH_ISL_FRU = SWITCH_ISL_BW + 1
     SWITCH_ISL_REDUNDANT = SWITCH_ISL_FRU + 1
+    SWITCH_FIRMWARE = SWITCH_ISL_REDUNDANT + 1
 
     # Port level alerts
     PORT_BASE = 400
@@ -152,7 +161,8 @@ class ALERT_NUM:
     REMOTE_PORT_L_TEMP_W = REMOTE_PORT_H_TEMP_A + 1
     REMOTE_PORT_L_TEMP_A = REMOTE_PORT_L_TEMP_W + 1
     LOGIN_SPEED_NOT_MAX_W = REMOTE_PORT_L_TEMP_A + 1
-    PORT_SFP_HAA_F16_32_P8 = LOGIN_SPEED_NOT_MAX_W + 1
+    SWITCH_LIMITED_SPEED = LOGIN_SPEED_NOT_MAX_W + 1
+    PORT_SFP_HAA_F16_32_P8 = SWITCH_LIMITED_SPEED + 1
     PORT_TXC3_DISCARD = PORT_SFP_HAA_F16_32_P8 + 1
     PORT_RXC3_DISCARD = PORT_TXC3_DISCARD + 1
     PORT_LOGICAL_ERRORS = PORT_RXC3_DISCARD + 1
@@ -204,6 +214,7 @@ class ALERT_NUM:
     ZONE_ADD_ZONE = ZONE_LINK_NO_ADDR + 1  # Newly created zone
     ZONE_KEPT = ZONE_ADD_ZONE + 1  # Existing zone kept
     ZONE_REMOVED = ZONE_KEPT + 1  # Zone removed/not needed
+    ALIAS_INITIATOR_UPPER = ZONE_REMOVED + 1
 
     # Chassis alerts
     CHASSIS_BASE = 700
@@ -215,6 +226,12 @@ class ALERT_NUM:
     IOCP_BASE = 800
     IOCP_MIXED_CU_TYPES = IOCP_BASE + 1
 
+    # General & customer alerts
+    GENERAL_BASE = 900
+    FREE_TEXT_INFO = GENERAL_BASE + 1
+    FREE_TEXT_WARN = FREE_TEXT_INFO + 1
+    FREE_TEXT_ERROR = FREE_TEXT_WARN + 1
+
 
 _power_above_threshold = ' above threshold. Threshold: $p1 uW. Actual: $p0 uW.'
 _power_below_threshold = ' below threshold. Threshold: $p1 uW. Actual: $p0 uW.'
@@ -224,7 +241,6 @@ _volt_above_threshold = ' above threshold. Threshold: $p1 V. Actual: $p0 V.'
 _volt_below_threshold = ' below threshold. Threshold: $p1 V. Actual: $p0 V.'
 _cur_above_threshold = ' above threshold. Threshold: $p1 mA. Actual: $p0 mA.'
 _cur_below_threshold = ' below threshold. Threshold: $p1 mA. Actual: $p0 mA.'
-
 
 class AlertTable:
     """
@@ -252,7 +268,7 @@ class AlertTable:
         ALERT_NUM.SWITCH_IDID: dict(m='Insistent Domain ID not set', s=al.ALERT_SEV.WARN,
                                     k='insistent-domain-id-enabled'),
         ALERT_NUM.SWITCH_FIRMWARE_8_2: dict(m='Firmware is $p0. It should be v8.2.1c or higher', s=al.ALERT_SEV.ERROR,
-                                            k='firmware-version'),
+                                            k='firmware-version'),  # Deprecated 6 Dec 2022
         ALERT_NUM.HANDLE_DUP_WWN: dict(m='Login enforcement must be 2. Refer to $key', s=al.ALERT_SEV.ERROR,
                                        k='f-port-login-settings/enforce-login'),
         ALERT_NUM.SWITCH_ISL_IMBALANCE: dict(m='ISL imbalance from $p0 to $p1', s=al.ALERT_SEV.ERROR, k='_isl'),
@@ -261,6 +277,7 @@ class AlertTable:
         ALERT_NUM.SWITCH_ISL_FRU: dict(m='ISLs on same slot from $p0 to $p1', s=al.ALERT_SEV.WARN, k='_isl'),
         ALERT_NUM.SWITCH_ISL_REDUNDANT: dict(m='Non-redundant ISL trunks from $p0 to $p1', s=al.ALERT_SEV.ERROR,
                                              k='_isl'),
+        ALERT_NUM.SWITCH_FIRMWARE: dict(m='Required minimum firmware is $p0. Actual is $p1', s=al.ALERT_SEV.ERROR),
 
         # Port
         ALERT_NUM.PORT_ENABLED_NO_LIGHT: dict(m='Enabled port has no logins', s=al.ALERT_SEV.GENERAL,
@@ -327,6 +344,8 @@ class AlertTable:
         ALERT_NUM.REMOTE_PORT_L_TEMP_W: dict(m='Remote SFP Temperature' + _temp_below_threshold, s=al.ALERT_SEV.WARN),
         ALERT_NUM.LOGIN_SPEED_NOT_MAX_W: dict(m='Port login speed is $p0G but link is capable of $p1G',
                                               s=al.ALERT_SEV.WARN),
+        ALERT_NUM.SWITCH_LIMITED_SPEED: dict(m='Attached device is capable of $p0G but switch SFP is limited to $p1G',
+                                             s=al.ALERT_SEV.WARN),
 
         # Login
         ALERT_NUM.LOGIN_DUP_LOGIN: dict(m='Duplicate WWN. Found in ', s=al.ALERT_SEV.ERROR),
@@ -384,6 +403,8 @@ class AlertTable:
         ALERT_NUM.ZONE_KEPT: dict(m='Kept. $p0', s=al.ALERT_SEV.GENERAL),
         ALERT_NUM.ZONE_REMOVED: dict(m='Removed. $p0', s=al.ALERT_SEV.GENERAL),
         ALERT_NUM.ZONE_UNDEFINED_ALIAS: dict(m='Alias $p0 used in zone $p1 does not exist.', s=al.ALERT_SEV.ERROR),
+        ALERT_NUM.ALIAS_INITIATOR_UPPER: dict(m='Alias for initiator contains upercase characters.',
+                                              s=al.ALERT_SEV.WARN),
 
         # Chassis
         ALERT_NUM.PORT_SFP_HAA_F16_32_P8: dict(
@@ -397,6 +418,12 @@ class AlertTable:
         # IOCP
         ALERT_NUM.IOCP_MIXED_CU_TYPES: dict(
             m='Mixed control unit types on the same CHPID, $p0. Control unit types: $p1', s=al.ALERT_SEV.WARN),
+
+        # General & customer alerts
+        # f=True is used in brcddb.report.zone to determine if login or port alerts should be displayed
+        ALERT_NUM.FREE_TEXT_INFO: dict(m='$p1', s=al.ALERT_SEV.GENERAL, f=True),
+        ALERT_NUM.FREE_TEXT_WARN: dict(m='$p1', s=al.ALERT_SEV.WARN, f=True),
+        ALERT_NUM.FREE_TEXT_ERROR: dict(m='$p1', s=al.ALERT_SEV.ERROR, f=True),
     }
     zone_alert_nums = {  # Application specific zone alerts intentionally left out of this list
         # ZONE
@@ -405,3 +432,146 @@ class AlertTable:
         'login_max_zone_participation': ALERT_NUM.LOGIN_MAX_ZONE_PARTICIPATION,
     }
     maps_alerts = (ALERT_NUM.MAPS_DASH_INFO, ALERT_NUM.MAPS_DASH_WARN, ALERT_NUM.MAPS_DASH_ERROR)
+
+lookup_d = dict(
+    # MAPS
+    MAPS_DASH_ERROR=ALERT_NUM.MAPS_DASH_ERROR,
+    MAPS_DASH_WARN=ALERT_NUM.MAPS_DASH_WARN,
+    MAPS_DASH_INFO=ALERT_NUM.MAPS_DASH_INFO,
+
+    # Project
+    PROJ_DUP_LOGIN=ALERT_NUM.PROJ_DUP_LOGIN,
+    PROJ_FAILED_LOGIN=ALERT_NUM.PROJ_FAILED_LOGIN,
+    PROJ_CHASSIS_API_ERROR=ALERT_NUM.PROJ_CHASSIS_API_ERROR,
+    PROJ_SWITCH_API_ERROR=ALERT_NUM.PROJ_SWITCH_API_ERROR,
+    PROJ_PROGRAM_ERROR=ALERT_NUM.PROJ_PROGRAM_ERROR,
+    PROJ_USER_ERROR=ALERT_NUM.PROJ_USER_ERROR,
+
+    # Switch
+    SWITCH_IDID=ALERT_NUM.SWITCH_IDID,
+    SWITCH_FIRMWARE_8_2=ALERT_NUM.SWITCH_FIRMWARE_8_2,
+    HANDLE_DUP_WWN=ALERT_NUM.HANDLE_DUP_WWN,
+    SWITCH_ISL_IMBALANCE=ALERT_NUM.SWITCH_ISL_IMBALANCE,
+    SWITCH_ISL_BW=ALERT_NUM.SWITCH_ISL_BW,
+    SWITCH_ISL_FRU=ALERT_NUM.SWITCH_ISL_FRU,
+    SWITCH_ISL_REDUNDANT=ALERT_NUM.SWITCH_ISL_REDUNDANT,
+
+    # Port
+    PORT_ENABLED_NO_LIGHT=ALERT_NUM.PORT_ENABLED_NO_LIGHT,
+    PORT_C3_DISCARD=ALERT_NUM.PORT_C3_DISCARD,
+    PORT_TXC3_DISCARD=ALERT_NUM.PORT_TXC3_DISCARD,
+    PORT_RXC3_DISCARD=ALERT_NUM.PORT_RXC3_DISCARD,
+    PORT_LOGICAL_ERRORS=ALERT_NUM.PORT_LOGICAL_ERRORS,
+    PORT_BIT_ERRORS=ALERT_NUM.PORT_BIT_ERRORS,
+    PORT_FRAME_ERRORS=ALERT_NUM.PORT_FRAME_ERRORS,
+    PORT_F_ZERO_CREDIT=ALERT_NUM.PORT_F_ZERO_CREDIT,
+    PORT_TSB_2019_274_WARN=ALERT_NUM.PORT_TSB_2019_274_WARN,
+    PORT_TSB_2019_274_ALERT=ALERT_NUM.PORT_TSB_2019_274_ALERT,
+    PORT_TSB_2019_276=ALERT_NUM.PORT_TSB_2019_276,
+    PORT_H_TXP_A=ALERT_NUM.PORT_H_TXP_A,
+    PORT_H_TXP_W=ALERT_NUM.PORT_H_TXP_W,
+    PORT_L_TXP_A=ALERT_NUM.PORT_L_TXP_A,
+    PORT_L_TXP_W=ALERT_NUM.PORT_L_TXP_W,
+    PORT_H_RXP_A=ALERT_NUM.PORT_H_RXP_A,
+    PORT_H_RXP_W=ALERT_NUM.PORT_H_RXP_W,
+    PORT_L_RXP_A=ALERT_NUM.PORT_L_RXP_A,
+    PORT_L_RXP_W=ALERT_NUM.PORT_L_RXP_W,
+    PORT_H_CUR_A=ALERT_NUM.PORT_H_CUR_A,
+    PORT_H_CUR_W=ALERT_NUM.PORT_H_CUR_W,
+    PORT_L_CUR_A=ALERT_NUM.PORT_L_CUR_A,
+    PORT_L_CUR_W=ALERT_NUM.PORT_L_CUR_W,
+    PORT_H_VLT_A=ALERT_NUM.PORT_H_VLT_A,
+    PORT_H_VLT_W=ALERT_NUM.PORT_H_VLT_W,
+    PORT_L_VLT_A=ALERT_NUM.PORT_L_VLT_A,
+    PORT_L_VLT_W=ALERT_NUM.PORT_L_VLT_W,
+    PORT_H_TEMP_A=ALERT_NUM.PORT_H_TEMP_A,
+    PORT_H_TEMP_W=ALERT_NUM.PORT_H_TEMP_W,
+    PORT_L_TEMP_A=ALERT_NUM.PORT_L_TEMP_A,
+    PORT_L_TEMP_W=ALERT_NUM.PORT_L_TEMP_W,
+    PORT_FAULT=ALERT_NUM.PORT_FAULT,
+    PORT_SEGMENTED=ALERT_NUM.PORT_SEGMENTED,
+    PORT_QSFP_BRKOUT_ASN=ALERT_NUM.PORT_QSFP_BRKOUT_ASN,
+    REMOTE_PORT_H_TXP_A=ALERT_NUM.REMOTE_PORT_H_TXP_A,
+    REMOTE_PORT_H_TXP_W=ALERT_NUM.REMOTE_PORT_H_TXP_W,
+    REMOTE_PORT_L_TXP_A=ALERT_NUM.REMOTE_PORT_L_TXP_A,
+    REMOTE_PORT_L_TXP_W=ALERT_NUM.REMOTE_PORT_L_TXP_W,
+    REMOTE_PORT_H_RXP_A=ALERT_NUM.REMOTE_PORT_H_RXP_A,
+    REMOTE_PORT_H_RXP_W=ALERT_NUM.REMOTE_PORT_H_RXP_W,
+    REMOTE_PORT_L_RXP_A=ALERT_NUM.REMOTE_PORT_L_RXP_A,
+    REMOTE_PORT_L_RXP_W=ALERT_NUM.REMOTE_PORT_L_RXP_W,
+    REMOTE_PORT_H_CUR_A=ALERT_NUM.REMOTE_PORT_H_CUR_A,
+    REMOTE_PORT_H_CUR_W=ALERT_NUM.REMOTE_PORT_H_CUR_W,
+    REMOTE_PORT_L_CUR_A=ALERT_NUM.REMOTE_PORT_L_CUR_A,
+    REMOTE_PORT_L_CUR_W=ALERT_NUM.REMOTE_PORT_L_CUR_W,
+    REMOTE_PORT_H_VLT_A=ALERT_NUM.REMOTE_PORT_H_VLT_A,
+    REMOTE_PORT_H_VLT_W=ALERT_NUM.REMOTE_PORT_H_VLT_W,
+    REMOTE_PORT_L_VLT_A=ALERT_NUM.REMOTE_PORT_L_VLT_A,
+    REMOTE_PORT_L_VLT_W=ALERT_NUM.REMOTE_PORT_L_VLT_W,
+    REMOTE_PORT_H_TEMP_A=ALERT_NUM.REMOTE_PORT_H_TEMP_A,
+    REMOTE_PORT_H_TEMP_W=ALERT_NUM.REMOTE_PORT_H_TEMP_W,
+    REMOTE_PORT_L_TEMP_A=ALERT_NUM.REMOTE_PORT_L_TEMP_A,
+    REMOTE_PORT_L_TEMP_W=ALERT_NUM.REMOTE_PORT_L_TEMP_W,
+    LOGIN_SPEED_NOT_MAX_W=ALERT_NUM.LOGIN_SPEED_NOT_MAX_W,
+    SWITCH_LIMITED_SPEED=ALERT_NUM.SWITCH_LIMITED_SPEED,
+
+    # Login
+    LOGIN_DUP_LOGIN=ALERT_NUM.LOGIN_DUP_LOGIN,
+    LOGIN_NOT_ZONED=ALERT_NUM.LOGIN_NOT_ZONED,
+    LOGIN_BASE_ZONED=ALERT_NUM.LOGIN_BASE_ZONED,
+    LOGIN_MAX_ZONE_PARTICIPATION=ALERT_NUM.LOGIN_MAX_ZONE_PARTICIPATION,
+    LOGIN_SIM=ALERT_NUM.LOGIN_SIM,
+    LOGIN_AMP=ALERT_NUM.LOGIN_AMP,
+    LOGIN_FDMI_NOT_ENABLED=ALERT_NUM.LOGIN_FDMI_NOT_ENABLED,
+    LOGIN_MIXED_SPEED_T=ALERT_NUM.LOGIN_MIXED_SPEED_T,
+    LOGIN_FASTER_S=ALERT_NUM.LOGIN_FASTER_S,
+
+    # Deprecated
+    LOGIN_SPEED_DIFF_W=ALERT_NUM.LOGIN_SPEED_DIFF_W,
+    LOGIN_SPEED_DIFF_E=ALERT_NUM.LOGIN_SPEED_DIFF_E,
+    LOGIN_SPEED_IMP_W=ALERT_NUM.LOGIN_SPEED_IMP_W,
+    LOGIN_SPEED_IMP_E=ALERT_NUM.LOGIN_SPEED_IMP_E,
+
+    # Zoning
+    # Zones
+    ZONE_NO_MEMBERS=ALERT_NUM.ZONE_NO_MEMBERS,
+    ZONE_ONE_MEMBER=ALERT_NUM.ZONE_ONE_MEMBER,
+    ZONE_PEER_NO_PMEM=ALERT_NUM.ZONE_PEER_NO_PMEM,
+    ZONE_PEER_NO_NMEM=ALERT_NUM.ZONE_PEER_NO_NMEM,
+    ZONE_MIXED=ALERT_NUM.ZONE_MIXED,
+    ZONE_WWN_ALIAS=ALERT_NUM.ZONE_WWN_ALIAS,
+    ZONE_NOT_USED=ALERT_NUM.ZONE_NOT_USED,
+    ZONE_MISMATCH=ALERT_NUM.ZONE_MISMATCH,
+    ZONE_PEER_PROPERTY=ALERT_NUM.ZONE_PEER_PROPERTY,
+    ZONE_LINK_ADDR=ALERT_NUM.ZONE_LINK_ADDR,
+    ZONE_LINK_NO_ADDR=ALERT_NUM.ZONE_LINK_NO_ADDR,
+    ZONE_ALIAS_USE=ALERT_NUM.ZONE_ALIAS_USE,
+    ZONE_PROB_AMP=ALERT_NUM.ZONE_PROB_AMP,
+    ZONE_DIFF_FABRIC=ALERT_NUM.ZONE_DIFF_FABRIC,
+    ZONE_NOT_FOUND=ALERT_NUM.ZONE_NOT_FOUND,
+    ZONE_BASE_ZONED=ALERT_NUM.ZONE_BASE_ZONED,
+    ZONE_MAX_PARTICIPATION=ALERT_NUM.ZONE_MAX_PARTICIPATION,
+    ZONE_DUP_ALIAS=ALERT_NUM.ZONE_DUP_ALIAS,
+    ZONE_NULL_ALIAS=ALERT_NUM.ZONE_NULL_ALIAS,
+    ZONE_NULL_ALIAS_USED=ALERT_NUM.ZONE_NULL_ALIAS_USED,
+    ZONE_ALIAS_NOT_USED=ALERT_NUM.ZONE_ALIAS_NOT_USED,
+    ZONE_MULTI_INITIATOR=ALERT_NUM.ZONE_MULTI_INITIATOR,
+    ZONE_ADD_ZONE=ALERT_NUM.ZONE_ADD_ZONE,
+    ZONE_KEPT=ALERT_NUM.ZONE_KEPT,
+    ZONE_REMOVED=ALERT_NUM.ZONE_REMOVED,
+    ZONE_UNDEFINED_ALIAS=ALERT_NUM.ZONE_UNDEFINED_ALIAS,
+    ALIAS_INITIATOR_UPPER=ALERT_NUM.ALIAS_INITIATOR_UPPER,
+
+    # Chassis
+    PORT_SFP_HAA_F16_32_P8=ALERT_NUM.PORT_SFP_HAA_F16_32_P8,
+    CHASSIS_FRU=ALERT_NUM.CHASSIS_FRU,
+    CHASSIS_TEMP_ERROR=ALERT_NUM.CHASSIS_TEMP_ERROR,
+    CHASSIS_TEMP_WARN=ALERT_NUM.CHASSIS_TEMP_WARN,
+
+    # IOCP
+    IOCP_MIXED_CU_TYPES=ALERT_NUM.IOCP_MIXED_CU_TYPES,
+
+    # General & customer alerts
+    FREE_TEXT_INFO=ALERT_NUM.FREE_TEXT_INFO,
+    FREE_TEXT_WARN=ALERT_NUM.FREE_TEXT_WARN,
+    FREE_TEXT_ERROR=ALERT_NUM.FREE_TEXT_ERROR,
+)
