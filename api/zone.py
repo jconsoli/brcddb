03ae488f0cf529f2e37e56160df490cda6ea9d17
Jack Consoli <jack@consoli-solutions.com>
2025-10-20 06:14:59 -0400

    Add files via upload

--- api/zone.py
+++ api/zone.py
@@ -9,7 +9,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 language governing permissions and limitations under the License.
 
 The license is free for single customer use (internal applications). Use of this module in the production,
-redistribution, or service delivery for commerce requires an additional license. Contact jack@consoli-solutions.com for
+redistribution, or service delivery for commerce requires an additional license. Contact jack_consoli@yahoo.com for
 details.
 
 **Description**
@@ -62,15 +62,17 @@ Typically, only replace_zoning() and enable_zonecfg() are called externally.
 +-----------+---------------+---------------------------------------------------------------------------------------+
 | 4.0.5     | 25 Aug 2025   | Updated email address in __email__ only.                                              |
 +-----------+---------------+---------------------------------------------------------------------------------------+
+| 4.0.6     | 19 Oct 2025   | Updated comments only.                                                                |
++-----------+---------------+---------------------------------------------------------------------------------------+
 """
 __author__ = 'Jack Consoli'
-__copyright__ = 'Copyright 2023, 2024, 2025 Consoli Solutions, LLC'
-__date__ = '25 Aug 2025'
+__copyright__ = 'Copyright 2024, 2025 Consoli Solutions, LLC'
+__date__ = '19 Oct 2025'
 __license__ = 'Apache License, Version 2.0'
 __email__ = 'jack_consoli@yahoo.com'
 __maintainer__ = 'Jack Consoli'
 __status__ = 'Released'
-__version__ = '4.0.5'
+__version__ = '4.0.6'
 
 import copy
 import sys
